440210403856f8cb20e140938130a4ff715fbf24
MatanBarhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-05-13 20:34:53 +0300

    Add files via upload

---
+++ Rename all folders.ps1
@@ -0,0 +1,7 @@
+﻿$path = Get-ChildItem "C:\Users\Admin\Desktop\v90 Project\maplev90-master\maplev90-master\server\dist\wz\*"
+ForEach ($item in $path.Name) 
+{
+   $NewItem = $item -replace ".wz",""
+   Rename-Item -Path "C:\Users\Admin\Desktop\v90 Project\maplev90-master\maplev90-master\server\dist\wz\$item" -NewName $NewItem
+   
+}
