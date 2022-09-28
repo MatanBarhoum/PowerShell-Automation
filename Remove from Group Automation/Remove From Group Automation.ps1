56e799719d233e82eeb939e8eb0180e73596998c
Matan Barhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-09-28 14:14:38 +0300

    Add files via upload

---
+++ Remove from Group Automation/Remove From Group Automation.ps1
@@ -0,0 +1,11 @@
+﻿$PermUsers = @(<names>);
+$Users = Get-ADGroupMember -Identity <GroupName> | Select -ExpandProperty SamAccountName
+
+
+for ($i = 0; $i -lt $Users.Count; $i++)
+{
+    for ($j = 0; $j -lt $PermUsers.Count; $j++)
+    {
+        if ($PermUsers -notcontains $Users[$i]) { Remove-ADGroupMember -Identity "Smart Card-Non Require" -Members $Users[$i] -Confirm:$false; break; }
+    }
+}
